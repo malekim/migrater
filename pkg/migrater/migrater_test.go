014248147f0989f322d17ee22f7501367d77a42a
Łukasz Małek <dev@malek.im>
2020-06-15 14:28:22 +0200

    Update migrater_test.go

--- pkg/migrater/migrater_test.go
+++ pkg/migrater/migrater_test.go
@@ -1,12 +1,15 @@
 package migrater
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"reflect"
 	"testing"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func TestNewMigrater(t *testing.T) {
@@ -39,5 +42,25 @@ func TestAddMongoMigration(t *testing.T) {
 
 	if len(m.mongo.migrations) == 0 {
 		t.Fatal("Expected", len(m.mongo.migrations), "Got", 0)
+	}
+}
+
+func TestSetMongoDatabase(t *testing.T) {
+	m := NewMigrater()
+	mongoURI := fmt.Sprintf("mongodb://%s:%s", os.Getenv("MONGO_HOST"), os.Getenv("MONGO_PORT"))
+	ctx := context.Background()
+	clientOpts := options.Client().ApplyURI(mongoURI)
+	client, err := mongo.Connect(ctx, clientOpts)
+	if err != nil {
+		t.Fatal("Unable to connect to Mongo")
+	}
+	if m.mongo.db != nil {
+		t.Fatal("Mongo should not be set")
+	}
+	db := client.Database("migrater")
+	m.SetMongoDatabase(db)
+
+	if m.mongo.db == nil {
+		t.Fatal("Mongo is not set properly")
 	}
 }
